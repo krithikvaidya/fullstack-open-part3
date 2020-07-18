085a3563872a4ac446af3605ff0ba5902d3db7e9
krithikvaidya <krithikvaidya@gmail.com>
2020-07-18 17:28:23 +0530

    Add frontend production build to backend repo

---
+++ build/precache-manifest.57f49b64293b5b9bcc90748d3174689f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24e56ef465a07ae390d916c11857da1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5c0a7ff8a5d8e9afdd5",
+    "url": "/static/css/main.54db7059.chunk.css"
+  },
+  {
+    "revision": "c164db0d716c10a5f408",
+    "url": "/static/js/2.f7f5c33f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f7f5c33f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5c0a7ff8a5d8e9afdd5",
+    "url": "/static/js/main.5f41afb1.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
