1b704cf8bf01fb126e8e7400dbe537a4acc87715
krithikvaidya <krithikvaidya@gmail.com>
2020-07-18 17:08:17 +0530

    Files to deploy

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express ()
 
@@ -8,7 +9,7 @@ morgan.token('body', req => {
     return req.method === "POST" ? JSON.stringify(req.body) : null
 })
 
-
+app.use (cors())
 app.use (morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use (express.json())
 
@@ -119,7 +120,7 @@ app.post ('/api/persons', (req, res) => {
 })
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT)
 console.log (`Server running on port ${PORT}`)
 
